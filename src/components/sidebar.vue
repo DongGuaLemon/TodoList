10a9234da0dae99bcf99f0e8ea98a4a0858fe463
ZongWei <g0970216929@gmail.com>
2018-11-26 00:28:03 +0800

    sidebar

---
+++ src/components/sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+<nav id="fixed-sidebar">
+  <div class="col-sm-12">
+    <!-- <a href="index.html"><img id="home-logo" src="../media/logo-prueba.jpg" alt="Logo de Animanoir"></a>  -->
+        <ul style="width:100%">
+          <li class="showall"><a  @click="showstatus1">全部</a></li>
+          <li class="showend"><a  @click="showstatus2">已完成</a></li>
+          <li class="showing"><a  @click="showstatus3">未完成</a></li>
+       </ul>
+  </div>
+</nav>  
+</template>
+
+<script>
+export default {
+  methods:{
+    showstatus1(){
+      this.$emit('showstatus1')
+    },
+    showstatus2(){
+      this.$emit('showstatus2')
+    },
+    showstatus3(){
+      this.$emit('showstatus3')
+    }
+  }
+}
+</script>
+<style scoped>
+#fixed-sidebar{
+  position: fixed;
+  max-width: 20%;
+  height: 100%;
+  background-color: #3498db;
+}
+ul > li{
+ margin:0 auto;
+ text-align: center;
+ margin-left: -50%;
+ list-style: none;
+ margin-top:20%; 
+}
+li:hover{
+  color: white;
+  cursor: pointer;
+}
+</style>
